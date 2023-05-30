8a8b88586256d48d4be5018de54daab52b4c79f9
Jim Chien <jim_chien_br@hotmail.com>
2023-05-29 21:58:58 -0300

    refactor: from plug to packer without code related plugins

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -4,9 +4,26 @@ if (not status) then
   return
 end
 
-vim.cmd [[packadd packer.nvim]]
+vim.cmd([[
+  packadd packer.nvim
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerCompile
+  augroup end
+]])
 
 packer.startup(function(use)
   use 'wbthomason/packer.nvim'
   -- Your plugins go here
+  -- Appearance
+  use 'vim-airline/vim-airline'
+  use 'haya14busa/is.vim'
+  use 'drewtempelmeyer/palenight.vim'
+  use 'vim-airline/vim-airline-themes'
+  use 'overcache/NeoSolarized'
+  use 'tpope/vim-commentary'
+  use { 'mg979/vim-visual-multi', branch = 'master' }
+  -- Git
+  use 'airblade/vim-gitgutter'
+  use 'tpope/vim-fugitive'
 end)
