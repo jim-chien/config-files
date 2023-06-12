44616fcf9287f069d3dd15c58434f20e36fed39a
Jim Chien <jim_chien_br@hotmail.com>
2023-06-12 12:35:50 -0300

    feat: config parsers for js and ts treesitter

--- .config/nvim/after/plugin/treesitter.rc.lua
+++ .config/nvim/after/plugin/treesitter.rc.lua
@@ -30,3 +30,6 @@ ts.setup {
     enable_autocmd = false,
   }
 }
+
+-- local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
+-- parser_config.tsx.filetype_to_parsername = { "javascript", "typescript.tsx" }
