feb194998811ab4f311264770b4206e4b95110d0
Jim Chien <jim_chien_br@hotmail.com>
2023-08-16 13:06:01 -0300

    fix: changed prettierd to prettier

--- .config/nvim/after/plugin/null-ls.rc.lua
+++ .config/nvim/after/plugin/null-ls.rc.lua
@@ -16,7 +16,9 @@ null_ls.setup({
   sources = {
     null_ls.builtins.formatting.stylua,
     null_ls.builtins.formatting.black,
-    null_ls.builtins.formatting.prettierd,
+    null_ls.builtins.formatting.prettier.with({
+      prefer_local = 'node_modules/.bin',
+    }),
     null_ls.builtins.diagnostics.mypy,
     null_ls.builtins.diagnostics.zsh,
     null_ls.builtins.diagnostics.eslint_d.with({
