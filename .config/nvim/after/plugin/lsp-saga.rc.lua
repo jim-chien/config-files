ba3437f87ce51e5cfc600a30346a5f86c9f02e7e
Jim Chien <jim_chien_br@hotmail.com>
2023-09-25 10:10:30 -0300

    fix: updated lsp finder keymap to goto definition in lsp saga

--- .config/nvim/after/plugin/lsp-saga.rc.lua
+++ .config/nvim/after/plugin/lsp-saga.rc.lua
@@ -3,12 +3,11 @@ if (not status) then return end
 
 saga.setup()
 
-local diagnostic = require('lspsaga.diagnostic')
 local opts = { noremap = true, silent = true }
 vim.keymap.set('n', 'gl', '<Cmd>Lspsaga show_line_diagnostics<CR>', opts)
 vim.keymap.set('n', 'gy', '<cmd>Lspsaga peek_type_definition<CR>')
 vim.keymap.set('n', 'K', '<Cmd>Lspsaga hover_doc<CR>', opts)
-vim.keymap.set('n', 'gd', '<Cmd>Lspsaga lsp_finder<CR>', opts)
+vim.keymap.set('n', 'gd', '<cmd>Lspsaga goto_definition<CR>', opts)
 vim.keymap.set('n', 'gD', '<cmd>Lspsaga goto_definition<CR>', opts)
 vim.keymap.set('n', 'gp', '<Cmd>Lspsaga peek_definition<CR>', opts)
 vim.keymap.set('n', '<leader>rn', '<Cmd>Lspsaga rename<CR>', opts)
