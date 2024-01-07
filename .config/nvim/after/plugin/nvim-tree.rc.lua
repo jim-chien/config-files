3e3317af679aa8faf68dde449ceea0fb2344dc66
Jim Chien <jim_chien_br@hotmail.com>
2024-01-07 19:32:45 -0300

    feat: changed root path in nvim tree appearance

--- .config/nvim/after/plugin/nvim-tree.rc.lua
+++ .config/nvim/after/plugin/nvim-tree.rc.lua
@@ -1,17 +1,19 @@
-local status, tree = pcall(require, 'nvim-tree')
-if (not status) then return end
+local status, tree = pcall(require, "nvim-tree")
+if not status then
+  return
+end
 
-local api = require('nvim-tree.api')
+local api = require("nvim-tree.api")
 
 local function on_attach(bufnr)
   -- use all default mappings
   api.config.mappings.default_on_attach(bufnr)
 
   -- remove a default
-  vim.keymap.del('n', 's', { buffer = bufnr, noremap = true, silent = true, nowait = true })
+  vim.keymap.del("n", "s", { buffer = bufnr, noremap = true, silent = true, nowait = true })
 end
 
-tree.setup {
+tree.setup({
   on_attach = on_attach,
   update_focused_file = {
     enable = true,
@@ -22,12 +24,29 @@ tree.setup {
   },
   renderer = {
     add_trailing = true,
+    group_empty = false,
+    highlight_git = true,
+    full_name = false,
+    highlight_opened_files = "none",
+    root_folder_label = ":t",
+    indent_width = 2,
     indent_markers = {
-      enable = true,
-    }
+      enable = false,
+      inline_arrows = true,
+      icons = {
+        corner = "└",
+        edge = "│",
+        item = "│",
+        none = " ",
+      },
+    },
   },
-}
+  view = {
+    centralize_selection = true,
+    width = 40,
+  },
+})
 
 vim.g.nvim_tree_respect_buf_cwd = 1
 
-vim.keymap.set('n', '<C-b>', ':NvimTreeToggle<CR>')
+vim.keymap.set("n", "<C-b>", ":NvimTreeToggle<CR>")
