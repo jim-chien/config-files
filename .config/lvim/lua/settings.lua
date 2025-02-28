bcf9e3d3d0e603aa782cf56c8fc1ef7ac749a064
Jim Chien <jim_chien_br@hotmail.com>
2025-02-28 12:22:29 -0300

    chore: add nvim tree to the right

--- .config/lvim/lua/settings.lua
+++ .config/lvim/lua/settings.lua
@@ -7,11 +7,21 @@ lvim.builtin.telescope.defaults.layout_config.height = 0.9
 lvim.builtin.telescope.defaults.layout_config.width = 0.8
 lvim.builtin.telescope.defaults.border = true
 lvim.builtin.telescope.defaults.borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" }
+lvim.builtin.telescope.pickers.find_files = {
+  find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" },
+}
 lvim.builtin.which_key.mappings.b.f = {
   "<cmd>Telescope buffers<cr>",
   "Find"
 }
-vim.list_extend(lvim.builtin.cmp.sources, {
-  { name = "codeium", priority = 100 }
-})
-lvim.builtin.cmp.formatting.source_names['codeium'] = "(Codeium)"
+lvim.builtin.nvimtree.setup.view.side = 'right'
+lvim.builtin.nvimtree.setup.view.adaptive_size = true
+
+-- vim.list_extend(lvim.builtin.cmp.sources, {
+--   {
+--     name = "codeium",
+--     max_item_count = 3,
+--     priority = 10,
+--   }
+-- })
+-- lvim.builtin.cmp.formatting.source_names['codeium'] = "(Codeium)"
