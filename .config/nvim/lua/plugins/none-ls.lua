3254be6f3e2ded47234d3a73b21ecead3f9066ed
Jim Chien <jim_chien_br@hotmail.com>
2025-05-10 18:47:20 -0300

    improve none ls config for eslint

--- .config/nvim/lua/plugins/none-ls.lua
+++ .config/nvim/lua/plugins/none-ls.lua
@@ -1,3 +1,15 @@
+local function has_eslint_config(utils)
+  return utils.root_has_file {
+    '.eslintrc',
+    '.eslintrc.cjs',
+    '.eslintrc.js',
+    '.eslintrc.json',
+    'eslint.config.cjs',
+    'eslint.config.js',
+    'eslint.config.mjs',
+  }
+end
+
 return {
   'nvimtools/none-ls.nvim',
   -- Copied from LazyVim/lua/lazyvim/util/plugin.lua
@@ -9,6 +21,7 @@ return {
   },
   opts = function()
     local nls = require 'null-ls'
+    vim.env.ESLINT_D_PPID = vim.fn.getpid()
 
     return {
       root_dir = require('null-ls.utils').root_pattern('.null-ls-root', '.neoconf.json', 'Makefile', '.git'),
@@ -18,8 +31,32 @@ return {
           prefer_local = 'node_modules/.bin',
         },
         nls.builtins.formatting.stylua,
-        require 'none-ls.diagnostics.eslint_d',
-        require 'none-ls.code_actions.eslint_d',
+        require('none-ls.diagnostics.eslint_d').with {
+          condition = has_eslint_config,
+          filetypes = {
+            'javascript',
+            'javascriptreact',
+            'typescript',
+            'typescriptreact',
+            'vue',
+            'svelte',
+            'astro',
+            'html',
+          },
+        },
+        require('none-ls.code_actions.eslint_d').with {
+          condition = has_eslint_config,
+          filetypes = {
+            'javascript',
+            'javascriptreact',
+            'typescript',
+            'typescriptreact',
+            'vue',
+            'svelte',
+            'astro',
+            'html',
+          },
+        },
       },
     }
   end,
