90282537f57ea2dc905db5a4d2da77405baa4bf6
Jim Chien <47145412+jim-chien@users.noreply.github.com>
2025-05-10 19:46:33 -0300

    style lualine for nvim

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -11,7 +11,7 @@ return {
         lualine_a = { 'mode' },
         lualine_b = { 'branch', 'diff', 'diagnostics' },
         lualine_c = { 'filename' },
-        lualine_x = { 'encoding', 'fileformat', 'filetype' },
+        lualine_x = { 'lsp_status', 'filetype' },
         lualine_y = { 'progress' },
         lualine_z = { 'location' },
       },
