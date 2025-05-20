7a185f5f4f46d4ebd4a0046b1e1b7ee64e5a2dd9
Jim Chien <47145412+jim-chien@users.noreply.github.com>
2025-05-20 12:08:27 -0300

    added border to completion blink floating window

--- .config/nvim/lua/plugins/blink-cmp.lua
+++ .config/nvim/lua/plugins/blink-cmp.lua
@@ -37,6 +37,7 @@ return {
     opts = {
       keymap = {
         preset = 'enter',
+        ['<CR>'] = { 'accept', 'select_and_accept', 'fallback' },
         ['<Tab>'] = { 'select_next', 'fallback' },
         ['<S-Tab>'] = { 'select_prev', 'fallback' },
         ['<C-n>'] = { 'snippet_forward', 'fallback' },
@@ -44,6 +45,7 @@ return {
       },
       completion = {
         menu = {
+          border = 'bold',
           draw = {
             columns = { { 'label', 'label_description', gap = 1 }, { 'kind_icon', 'kind' } },
             padding = { 0, 1 }, -- padding only on right side
@@ -57,7 +59,7 @@ return {
           },
         },
         list = {
-          selection = { preselect = false, auto_insert = true },
+          selection = { preselect = true, auto_insert = true },
           max_items = 10,
         },
         documentation = { auto_show = true, auto_show_delay_ms = 500 },
