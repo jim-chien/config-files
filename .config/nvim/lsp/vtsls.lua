23d470f1e771f266b60b5b6f8c1f9fdb1d18d7e1
Jim Chien <47145412+jim-chien@users.noreply.github.com>
2025-06-04 12:47:25 -0300

    improve vtsls config root dir and max ts server memory

--- .config/nvim/lsp/vtsls.lua
+++ .config/nvim/lsp/vtsls.lua
@@ -1,6 +1,24 @@
 return {
-  root_markers = { 'tsconfig.base.json', 'tsconfig.json', 'package.json', '.git' },
+  root_markers = { 'tsconfig.base.json', 'tsconfig.json', 'jsconfig.json', 'package.json', '.git' },
+  root_dir = function(bufnr, cb)
+    local fname = vim.uri_to_fname(vim.uri_from_bufnr(bufnr))
+
+    local ts_root = vim.fs.find('tsconfig.json', { upward = true, path = fname })[1]
+    -- Use the git root to deal with monorepos where TypeScript is installed in the root node_modules folder.
+    local git_root = vim.fs.find('.git', { upward = true, path = fname })[1]
+
+    if git_root then
+      cb(vim.fn.fnamemodify(git_root, ':h'))
+    elseif ts_root then
+      cb(vim.fn.fnamemodify(ts_root, ':h'))
+    end
+  end,
   settings = {
+    typescript = {
+      tsserver = {
+        maxTsServerMemory = 4096,
+      },
+    },
     vtsls = {
       tsserver = {
         globalPlugins = {
